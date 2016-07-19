8bb8fe2510977b5b9d27d6b7775f3216722dc280
Daniel Patterson <daniel@mapbox.com>
2016-07-19 13:13:52 +0200

    Remove generic std::exception handlers, they don't seem to be adding value, and they hide useful info.

--- src/extractor/extractor.cpp
+++ src/extractor/extractor.cpp
@@ -73,7 +73,9 @@ namespace extractor
  */
 int Extractor::run(ScriptingEnvironment &scripting_environment)
 {
-    try
+    // setup scripting environment
+    ScriptingEnvironment scripting_environment(config.profile_path.string().c_str());
+
     {
         util::LogPolicy::GetInstance().Unmute();
         TIMER_START(extracting);
@@ -202,14 +204,7 @@ int Extractor::run(ScriptingEnvironment &scripting_environment)
         util::SimpleLogger().Write() << "extraction finished after " << TIMER_SEC(extracting)
                                      << "s";
     }
-    // we do this for scoping
-    // TODO move to own functions
-    catch (const std::exception &e)
-    {
-        util::SimpleLogger().Write(logWARNING) << e.what();
-        return 1;
-    }
-    try
+
     {
         // Transform the node-based graph that OSM is based on into an edge-based graph
         // that is better for routing.  Every edge becomes a node, and every valid
@@ -265,11 +260,6 @@ int Extractor::run(ScriptingEnvironment &scripting_environment)
             << " nodes/sec and " << ((max_edge_id + 1) / TIMER_SEC(expansion)) << " edges/sec";
         util::SimpleLogger().Write() << "To prepare the data for routing, run: "
                                      << "./osrm-contract " << config.output_file_name << std::endl;
-    }
-    catch (const std::exception &e)
-    {
-        util::SimpleLogger().Write(logWARNING) << e.what();
-        return 1;
     }
 
     return 0;
